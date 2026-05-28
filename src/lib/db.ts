63009ed46106ce0d983c36f358988eaa8cddd9fa
zuniqcomputers-crypto <zuniqcomputers@gmail.com>
2026-05-28 10:27:41 +0300

    fix: global mongoose type

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 if (!MONGODB_URI) throw new Error("Please define MONGODB_URI in .env.local");
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
+// Fix: TypeScript doesn't like `global.mongoose` without declaration
+let cached = (global as any).mongoose;
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
 
 async function dbConnect() {
   if (cached.conn) return cached.conn;
